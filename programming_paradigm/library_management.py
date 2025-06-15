4269256338051d7e2f5a708845a10ff2b6400aea
Chibuikem Izuagwu <izuagwuchibuikem@gmail.com>
2025-06-15 23:18:26 +0100

    library_management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -22,12 +22,12 @@ class Library:
     
     def return_book(self, book):
         """Return a checked-out book to the library."""
-        if book in self.books and book.is_checked_out:
+        if book in self._books and book.is_checked_out:
             book.is_checked_out = False
             return True
         return False
     
     def list_books(self):
         """List all books in the library."""
-        return [(book.title, book.author, book.is_checked_out) for book in self.books]
+        return [(book.title, book.author, book.is_checked_out) for book in self._books]
         
