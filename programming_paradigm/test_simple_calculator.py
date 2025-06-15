d2007c094be53b8b9e80e1aa74b6e35a11b44652
Chibuikem Izuagwu <izuagwuchibuikem@gmail.com>
2025-06-15 22:30:11 +0100

    test_simple_calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,7 +8,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_add(self):
-        """Test the add method."""
+        """Test addition."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
@@ -16,7 +16,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
 
     def test_subtract(self):
-        """Test the subtract method."""
+        """Test subtraction."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(-3, -7), 4)
