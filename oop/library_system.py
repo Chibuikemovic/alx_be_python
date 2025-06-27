6a8a2dd79daf63d8eea97191fabfc8ab73886c20
Chibuikem Izuagwu <izuagwuchibuikem@gmail.com>
2025-06-27 22:49:58 +0100

    Library

---
+++ oop/library_system.py
@@ -0,0 +1,33 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class EBook(Book):
+    def __init__(self, title, author, file_size=0):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count=0):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+
+class Library:
+    
+    def __init__(self):
+        self.books = []
+        
+    def add_book(self, book):
+        self.book = book
+        self.books.append(book)
+        
+    def list_books(self):
+        for book in self.books:
+            print(f'{book.title} by {book.author}')
+            if isinstance(book, EBook):
+                print(f'File Size: {book.file_size} MB')
+            elif isinstance(book, PrintBook):
+                print(f'Page Count: {book.page_count}')
+        
